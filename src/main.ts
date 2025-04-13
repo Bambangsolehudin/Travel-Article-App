9c91c1e5ba8f232718b213e17fbcb9393ba5a830
Bambang Solhud <42888942+Bambangsolehudin@users.noreply.github.com>
2025-04-13 18:18:51 +0700

    merapihkan

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,3 @@
-// /**
-//  * main.ts
-//  *
-//  * Bootstraps Vuetify and other plugins then mounts the App`
-//  */
-
-// // Plugins
-// import { registerPlugins } from '@/plugins'
-
-// // Components
-// import App from './App.vue'
-
-// // Composables
-// import { createApp } from 'vue'
-// import './style.css'
-
-// const app = createApp(App)
-
-// registerPlugins(app)
-
-// app.mount('#app')
-
-
-
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
